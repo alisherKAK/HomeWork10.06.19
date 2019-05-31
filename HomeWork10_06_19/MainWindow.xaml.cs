b8495857f4501013e998d7065a3f28a1ce398549
Alisher Kalabaev <alisher.alish333@gmail.com>
2019-05-31 21:40:25 +0600

    Home Work

--- HomeWork10_06_19/MainWindow.xaml.cs
+++ HomeWork10_06_19/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Management;
 
 namespace HomeWork10_06_19
 {
@@ -20,9 +23,44 @@ namespace HomeWork10_06_19
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<ProcessDescription> _processDescriptions = new ObservableCollection<ProcessDescription>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            var processes = System.Diagnostics.Process.GetProcesses();
+            foreach(var process in processes)
+            {
+                ProcessDescription newProcessDescription = new ProcessDescription();
+                newProcessDescription.Name = process.ProcessName;
+                newProcessDescription.Id = process.Id;
+                newProcessDescription.State = process.Responding ? "Running" : "Stoped";
+
+                _processDescriptions.Add(newProcessDescription);
+            }
+
+            processesDataGrid.ItemsSource = _processDescriptions;
+        }
+
+        private string GetProcessOwner(int processId)
+        {
+            string query = "Select * From Win32_Process Where ProcessID = " + processId;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ManagementObjectCollection processList = searcher.Get();
+
+            foreach (ManagementObject obj in processList)
+            {
+                string[] argList = new string[] { string.Empty, string.Empty };
+                int returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
+                if (returnVal == 0)
+                {
+                    // return DOMAIN\user
+                    return argList[1] + "\\" + argList[0];
+                }
+            }
+
+            return "NO OWNER";
         }
     }
 }
---
+++ HomeWork10_06_19/ProcessDescription.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeWork10_06_19
+{
+    public class ProcessDescription
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string State { get; set; }
+        public string Owner { get; set; }
+        public string CPU { get; set; }
+        public double Memory { get; set; }
+        public string Despription { get; set; }
+    }
+}
